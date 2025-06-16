4c84d2cb7b7badd496d0b9e674854159b28ac931
anthony-j-g <anthonyjbasai@gmail.com>
2025-06-16 01:11:25 -0400

    [editor] Adding sample list box for the scene list window to build off of later

--- src/runtime/editor/scene_list_window.cpp
+++ src/runtime/editor/scene_list_window.cpp
@@ -27,7 +27,28 @@ void SceneListWindow::Update() {
 void SceneListWindow::Draw() {
 	if (!is_open) { return; }
 
-	ImGui::Begin("Scene List");
+	const char* items[] = { "AAAA", "BBBB", "CCCC", "DDDD", "EEEE", "FFFF", "GGGG", "HHHH", "IIII", "JJJJ", "KKKK", "LLLLLLL", "MMMM", "OOOOOOO" };
+
+	ImGui::Begin("Scene List");	
+	static int item_selected_idx = 0; // Here we store our selected data as an index.
+
+	if (ImGui::BeginListBox("listbox 1")) {
+		for (int n = 0; n < IM_ARRAYSIZE(items); n++) {
+			const bool is_selected = (item_selected_idx == n);
+			if (ImGui::Selectable(items[n], is_selected))
+				item_selected_idx = n;
+
+			// Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+			if (is_selected)
+				ImGui::SetItemDefaultFocus();
+		}
+	}
+    ImGui::EndListBox();
+
+	if (ImGui::Button("Load")) {
+		printf("Attempted to load scene: %s\n", items[item_selected_idx]);
+	}
+	
 	ImGui::End();
 }
 //---------------------------------------------------------------------------------- End Scene List Window
