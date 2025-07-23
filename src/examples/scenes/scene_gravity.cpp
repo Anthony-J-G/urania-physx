e34227b1c9737df18559832a00265081b9bf0af8
anthony-j-g <anthony-j-g@frostmarch>
2025-07-23 13:10:08 -0700

    [examples] adding another test scene (Earth/Moon System)

--- src/examples/scenes/scene_gravity.cpp
+++ src/examples/scenes/scene_gravity.cpp
@@ -4,6 +4,23 @@
 #include <stdio.h>
 
 
+class MoonEarth : public Scene {
+public:
+	enum {
+		e_count = 40
+	};
+
+	MoonEarth(const Settings& settings, int solverType)
+		: Scene(settings, solverType)
+	{
+	}
+
+	static Scene* Create(const Settings& settings, int solverType) {
+		return new MoonEarth(settings, solverType);
+	}
+};
+static int exampleMoonEarth = RegisterScene("Gravity", "Moon/Earth System", MoonEarth::Create);
+
 
 class NBody : public Scene {
 public:
@@ -20,4 +37,4 @@ public:
 		return new NBody(settings, solverType);
 	}
 };
-static int sampleFarChain = RegisterScene("Gravity", "N-Body", NBody::Create);
+static int exampleNBody = RegisterScene("Gravity", "N-Body", NBody::Create);
