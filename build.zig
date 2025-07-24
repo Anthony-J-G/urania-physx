561fd3905cceee29ae17f9831a790c90dd962c91
anthony-j-g <anthony-j-g@frostmarch>
2025-07-24 12:54:20 -0700

    [build.zig] adding additional build options

--- build.zig
+++ build.zig
@@ -17,30 +17,50 @@ pub const Options = struct {
     target: ResolvedTarget,
     optimize: OptimizeMode,
 
-    engine_is_packaged: bool = false,
-    should_build_tests: bool = true,
+    engine: bool = false,
+    shared: bool = false,
+    examples: bool = false,
+    headers_only: bool = false,
 };
-
 
 pub fn default_options(b: *std.Build) Options {
     return .{
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
-        .engine_is_packaged = b.option(bool, "recompile-from-engine", "for internal use by the engine only") orelse false,
-        .should_build_tests = b.option(bool, "tests", "Compile tests and add test command") orelse true,
+        .engine = b.option(bool, "engine", "Only compile the engine binary") orelse false,
+        .shared = b.option(bool, "shared", "Compile the engine binary as a shared object (dynamic library)") orelse false,
+        .examples = b.option(bool, "examples", "Only compile the examples executable") orelse false,
+        .headers_only = b.option(bool, "headers-only", "Only install the engine's header files") orelse false,
     };
 }
 
 
 pub fn build(b: *std.Build) void {
     const opts = default_options(b);
+    
+    const install_engine = (
+        opts.engine or (!opts.examples and !opts.engine)
+    ) and !opts.headers_only;
 
+    const install_examples = (
+        opts.examples or (!opts.examples and !opts.engine)
+    ) and !opts.headers_only;
+
+    // Installation
+    // -------------------------------
+    
     const engine_lib = engine.build(b, opts);
-    b.installArtifact(engine_lib);
+    if (install_engine) {
+        b.installArtifact(engine_lib);    
+    }
+    
+    if (install_examples) {
+        const examples_exe = examples.build(b, opts);
+        examples_exe.linkLibrary(engine_lib);
 
-    const examples_exe = examples.build(b, opts);
-    examples_exe.linkLibrary(engine_lib);
-    b.installArtifact(examples_exe);
+        b.installArtifact(examples_exe);
+    }
+    // ------------------------------- end installation  
 
     compiledb.generateFromInstallStep(b);
 }
